40d4f83bac89d72a69e92445389533c851d7dbec
Isaac <isaachrisadukwu@gmail.com>
2026-09-17 12:14:17 +0100

    added state bucket, lock, and encrypt

--- project-03-terraform-foundation/versions.tf
+++ project-03-terraform-foundation/versions.tf
@@ -2,11 +2,19 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  backend "s3" {
+    bucket       = "devops-tf-state-aduktech-2026-0916"
+    key          = "project-03/terraform.tfstate"
+    region       = "eu-west-2"
+    encrypt      = true
+    use_lockfile = true
+  }
+
   required_providers {
     random = {
-  source  = "hashicorp/random"
-  version = "~> 3.0"
-	}
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
@@ -33,3 +41,4 @@ resource "aws_s3_bucket_versioning" "artifacts" {
     status = "Enabled"
   }
 }
+
